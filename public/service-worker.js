4c39714a8f662b5ec4e12ce90706acf923bc111c
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-10-25 21:42:56 -0400

    added favicon and formatted service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,17 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "db.js",
-    "favicon.ico",
-    "index.js",
-    "manifest.webmanifest",
-    "service-worker.js",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
-    "styles.css"
-  ];
-​
+  "/",
+  "/index.html",
+  "db.js",
+  "favicon.ico",
+  "index.js",
+  "manifest.webmanifest",
+  "service-worker.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "styles.css",
+];
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
-​
 // install
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
@@ -36,10 +34,8 @@ self.addEventListener("install", function (evt) {
         console.log(err);
       })
   );
-​
   self.skipWaiting();
 });
-​
 // activate
 self.addEventListener("activate", function (evt) {
   evt.waitUntil(
@@ -54,10 +50,8 @@ self.addEventListener("activate", function (evt) {
       );
     })
   );
-​
   self.clients.claim();
 });
-​
 // fetch
 self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/")) {
@@ -71,7 +65,6 @@ self.addEventListener("fetch", function (evt) {
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
-​
               return response;
             })
             .catch((err) => {
@@ -81,10 +74,8 @@ self.addEventListener("fetch", function (evt) {
         })
         .catch((err) => console.log(err))
     );
-​
     return;
   }
-​
   evt.respondWith(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.match(evt.request).then((response) => {
